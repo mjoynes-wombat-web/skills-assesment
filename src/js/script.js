73ccf561ea3f3cf5f4d0657d27ac84c75044a22d
ssmith-wombatweb <ssmith@wombatweb.us>
2017-08-06 21:59:25 -0700

    Cleanup and refactoring.

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,23 @@
 window.onload = function () {
   var contactType = document.getElementById('contact-type').children[0];
+  contactType.addEventListener("change", function() {toggleContact(contactType.value)})
+};
 
-  contactType.addEventListener("change", function() {
+function toggleContact(value) {
+  var toShow = document.getElementsByClassName(value);
+  var toHide;
 
-    var phoneNumbers = document.getElementsByClassName('phone');
-    var emails = document.getElementsByClassName('email');
+  if (value === 'phone') {
+    toHide = document.getElementsByClassName('email');
+  } else if (value === 'email') {
+    toHide = document.getElementsByClassName('phone');
+  };
 
-    if (contactType.value === 'phone') {
+  for (i = 0; i < toShow.length; i++) {
+    toShow[i].style.display = "block";
+  };
 
-      for (i =0; i < phoneNumbers.length; i++) {
-        phoneNumbers[i].classList.remove('hidden')
-      }
-
-      for (i =0; i < emails.length; i++) {
-        emails[i].classList.add('hidden')
-      }
-    } else if (contactType.value === 'email') {
-
-      for (i =0; i < phoneNumbers.length; i++) {
-        phoneNumbers[i].classList.add('hidden')
-      }
-
-      for (i =0; i < emails.length; i++) {
-        emails[i].classList.remove('hidden')
-      }
-    }
-  })
-}
+  for (i = 0; i < toHide.length; i++) {
+    toHide[i].style.display = "none";
+  };
+};
